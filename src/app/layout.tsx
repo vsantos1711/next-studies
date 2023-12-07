ce79218cbf1af5e87adb349fcb1ca8ca6db74970
vsantos1711 <vsantos067100@gmail.com>
2023-12-06 21:35:36 -0300

    Add Providers component to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import StyledComponentsRegistry from '@/lib/registry';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Providers } from './providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        <Providers>
+          <StyledComponentsRegistry>{children}</StyledComponentsRegistry>{' '}
+        </Providers>
       </body>
     </html>
   );

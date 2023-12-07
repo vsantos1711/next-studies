264a3f53e5c29e9d0cb8e0d26cda6ba5bcdcd828
vsantos1711 <vsantos067100@gmail.com>
2023-12-06 22:09:26 -0300

    Add jest styled components config

--- jest.config.js
+++ jest.config.js
@@ -6,10 +6,16 @@ module.exports = {
     'src/**/*.ts(x)?',
     '!src/app/**',
     '!src/lib/registry.tsx',
+    '!src/types/**',
+    '!src/styles/**',
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
+  moduleNameMapper: {
+    'styled-components':
+      'styled-components/dist/styled-components.browser.cjs.js',
   },
 };

b9e1ca5a06cfcb4439e0985462dddb8c9e17d391
vsantos1711 <vsantos067100@gmail.com>
2023-12-09 11:41:35 -0300

    Add Storybook configuration

---
+++ .storybook/preview.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { GlobalStyle } from '../src/styles/global';
+
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyle />
+      <Story />
+    </>
+  ),
+];
---
+++ src/components/main/stories.tsx
@@ -0,0 +1,9 @@
+import { StoryObj, Meta } from '@storybook/react';
+import Main from '.';
+
+export default {
+  title: 'Main',
+  component: Main,
+} as Meta;
+
+export const Default: StoryObj = {};
